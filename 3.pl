5235ae81a11ccc3d8ea042c9b44da903eb1893e4
bendan <wuyarong525@126.com>
2013-04-10 14:57:11 +0800

    Init

---
+++ 3.pl
@@ -0,0 +1,23 @@
+open (FILE, "Mus_musculus.gene_info");
+open (LIST,"protein_geneid.txt");
+open (OUT, ">id_symbol3.txt");
+print OUT ("gene_id\tsymbol\n");
+%array=();
+$line=<FILE>;
+$line=<FILE>;
+
+@lili=<LIST>;
+chomp(@lili);
+$size=scalar(@lili);
+
+while($line ne "")
+{
+	@id=split(/\t/,$line);
+	$array{$id[1]}=$id[2];	
+  $line=<FILE>;
+}
+for($i=1;$i<$size;$i++)	
+	{
+  	@li=split(/\t/,$lili[$i]);
+  	print OUT ("$li[7]\t$array{$li[7]}\n");
+  }
---
+++ 4.pl
@@ -0,0 +1,23 @@
+open (FILE, "Mus_musculus.gene_info");
+open (LIST,"protein_geneid-tem.txt");
+open (OUT, ">id_symbol4.txt");
+print OUT ("gene_id\tsymbol\n");
+%array=();
+$line=<FILE>;
+$line=<FILE>;
+
+@lili=<LIST>;
+chomp(@lili);
+$size=scalar(@lili);
+
+while($line ne "")
+{
+	@id=split(/\t/,$line);
+	$array{$id[1]}=$id[2];	
+  $line=<FILE>;
+}
+for($i=1;$i<$size;$i++)	
+	{
+  	@li=split(/\t/,$lili[$i]);
+  	print OUT ("$li[6]\t$array{$li[6]}\n");
+  }
